cca60ff568d3b894b631d817392183aaf7a16afd
Alec Sharp <alecsharpie@gmail.com>
2022-02-01 01:51:09 +1100

    put connection and functions into api.py, create .env file and add to gitignore

--- api.py
+++ api.py
@@ -7,6 +7,70 @@ origins = [
 
 app = FastAPI()
 
+import pymongo
+
+from model import *
+import motor.motor_asyncio
+from dotenv import dotenv_values
+import os
+
+from http.client import HTTPException
+
+config = dotenv_values(".env")
+
+DATABASE_URI = config.get("DATABASE_URI")
+
+if os.getenv("DATABASE_URI"):
+    DATABASE_URI = os.getenv(
+        "DATABASE_URI"
+    )  #ensures that if we have a system environment variable, it uses that instead
+
+#client = motor.motor_asyncio.AsyncIOMotorClient(DATABASE_URI)
+
+client = pymongo.MongoClient(DATABASE_URI)
+db = client.test
+
+database = client.TodoDatabase
+collection = database.todos
+
+
+async def fetch_all_todos():
+    todos = []
+    cursor = collection.find()
+    async for doc in cursor:
+        todos.append(Todo(**doc))
+    return todos
+
+
+async def fetch_one_todo(nanoid):
+    doc = await collection.find_one({"nanoid": nanoid}, {"_id": 0})
+    return doc
+
+
+async def create_todo(todo):
+    doc = todo.dict()
+    await collection.insert_one(doc)
+    result = await fetch_one_todo(todo.nanoid)
+    return result
+
+
+async def change_todo(nanoid, title, desc, checked):
+    await collection.update_one(
+        {"nanoid": nanoid},
+        {"$set": {
+            "title": title,
+            "desc": desc,
+            "checked": checked
+        }})
+    result = await fetch_one_todo(nanoid)
+    return result
+
+
+async def remove_todo(nanoid):
+    await collection.delete_one({"nanoid": nanoid})
+    return True
+
+
 app.add_middleware(CORSMiddleware,
                    allow_origins=origins,
                    allow_credentials=True,
@@ -17,3 +81,38 @@ app.add_middleware(CORSMiddleware,
 @app.get('/')
 def get_root():
     return {"Ping": "Pong"}
+
+
+@app.get("/api/get-todo/{nanoid}", response_model=Todo)
+async def get_one_todo(nanoid):
+    todo = await fetch_one_todo(nanoid)
+    if not todo: raise HTTPException(404)
+    return todo
+
+
+@app.get("/api/get-todo")
+async def get_todos():
+    todos = await fetch_all_todos()
+    if not todos: raise HTTPException(404)
+    return todos
+
+
+@app.post("/api/add-todo", response_model=Todo)
+async def add_todo(todo: Todo):
+    result = await create_todo(todo)
+    if not result: raise HTTPException(400)
+    return result
+
+
+@app.put("/api/update-todo/{nanoid}", response_model=Todo)
+async def update_todo(todo: Todo):
+    result = await change_todo(nanoid, title, desc, checked)
+    if not result: raise HTTPException(400)
+    return result
+
+
+@app.delete("/api/delete-todo/{nanoid}")
+async def delete_todo(nanoid):
+    result = await remove_todo(nanoid)
+    if not result: raise HTTPException(400)
+    return result
